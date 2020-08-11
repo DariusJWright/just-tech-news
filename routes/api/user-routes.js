dbab4e0ea4b8a9fcbcf2b04c3dfabade59063985
darshnablah <dwright758@gmail.com>
2020-08-11 10:37:31 -0500

    added login functionallity

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,30 @@ router.post('/', (req, res) => {
         });
 });
 
+// login route
+router.post('/login', (req, res) => {
+    // expects {email, password}
+    User.findOne({
+        where: {
+        email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'root', email: 'root@root.com', password: 'password'}
@@ -83,7 +107,7 @@ router.delete('/:id', (req, res) => {
     })
         .then(dbUserData => {
             if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id '});
+                res.status(404).json({ message: 'No user found with this id ' });
                 return;
             }
             res.json(dbUserData);
