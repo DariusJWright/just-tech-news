36effea10fd9036293fc0ab9600441a96ae42694
darshnablah <dwright758@gmail.com>
2020-08-16 22:15:07 -0500

    homepage created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 
 const app = express();
@@ -8,9 +11,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// use handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
